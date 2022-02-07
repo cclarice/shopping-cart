8e938d24ffcefe7fa9944a4ad02785aa63502d54
dpetrov <dpetrov@smartdatatech.io>
2022-02-07 20:44:38 +0300

    adaptive

---
+++ src/App.vue
@@ -0,0 +1,251 @@
+<template>
+	<!--
+		Два элемента (h1, section) в корне компоненте т.к пишу на Vue 3.x
+		Знаком с тем что на Vue 2.x нельзя использовать более одного элемента в корне компонента
+	-->
+	<h1>
+		Корзина
+	</h1>
+	<section class="cart">
+		<button class="cart__add-button"
+            @click="add">
+			Добавить
+		</button>
+		<table class="cart__table">
+			<thead>
+				<tr>
+					<td>
+            Товар
+          </td>
+					<td>
+            Стоимость
+          </td>
+					<td>
+            Количество
+          </td>
+					<td/>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="item of cart" :key="item.id">
+					<td>
+            <base-select :options="products"
+                         :key="'name'"
+                         :default="products.indexOf(products.find((product) => product.name === item.name))"
+                         @select="select(item, $event)">
+              <template #selected="selected">
+                <div class="select__selected-option">
+                  {{ selected.selected.name }}
+                </div>
+              </template>
+              <template #option="option">
+                  {{ option.option.name }}
+              </template>
+            </base-select>
+					</td>
+					<td>
+						{{ item.price }}
+					</td>
+					<td>
+            <input type="number" @input="setAmount(item, $event)" :value="item.amount" min="1" max="2147483647" step="1">
+          </td>
+					<td>
+            <button @click="remove(item.id)">
+              Удалить
+            </button>
+          </td>
+				</tr>
+        <tr v-if="!cart.length">
+          <td> Пусто </td>
+          <td>
+            -
+          </td>
+          <td>
+            <input type="number" style="width: 113px; visibility: hidden">
+          </td>
+          <td>
+            <button style="visibility: hidden">
+              Удалить
+            </button>
+          </td>
+        </tr>
+				<tr>
+					<td/>
+					<td>
+            Итого:
+          </td>
+					<td>
+            {{ total }}
+          </td>
+					<td/>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+  <button class="theme-button" @click="toggleTheme">
+    <img :src="theme === 'dark'
+               ? require('@/assets/icons/theme/dark.svg')
+               : require('@/assets/icons/theme/light.svg')"
+         alt="theme">
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IProduct, IItem } from '@/models/product'
+import BaseSelect from '@/components/BaseSelect.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { BaseSelect },
+  data: (): { products: IProduct[], cart: IItem[], theme: string } => ({
+    products: [
+      { id: 1, name: 'Картофель', price: 20 },
+      { id: 2, name: 'Сок', price: 100 },
+      { id: 3, name: 'Мясо', price: 500 },
+      { id: 4, name: 'Бананы', price: 50 },
+    ],
+		cart: [],
+    theme: 'light'
+  }),
+	computed: {
+    total (): number {
+      return this.cart.reduce((prev, curr) => {
+        return prev + (curr.price * curr.amount)
+			}, 0)
+		}
+	},
+	methods: {
+		add (): void {
+      this.cart.push({
+        ...this.products[0],
+        id: this.cart.length,
+        amount: 1
+      })
+      this.saveCart()
+		},
+    select (item: IItem, selected: IProduct): void {
+      item.name = selected.name
+      item.price = selected.price
+      localStorage.setItem('cart', JSON.stringify(this.cart))
+    },
+		recalculateCartIds (from = 0): void {
+      for (let i = from; i < this.cart.length; i++) {
+        this.cart[i].id = i;
+			}
+		},
+		remove (id: number): void {
+      this.cart = this.cart.filter((item) => item.id !== id)
+			this.recalculateCartIds(id)
+      this.saveCart()
+		},
+    toggleTheme () {
+      localStorage.setItem('darkTheme', this.theme = (this.theme === 'light' ? 'dark' : 'light'))
+      document.documentElement.dataset.theme = this.theme
+    },
+    saveCart () {
+      localStorage.setItem('cart', JSON.stringify(this.cart))
+    },
+    setAmount (item: IItem, event: InputEvent) {
+      const target = (event.target as HTMLInputElement)
+
+      if (target?.value) {
+        const value = Number(target.value)
+        console.log(value)
+
+        if (target.value.includes('-')) {
+          target.value = String(item.amount)
+        }
+        item.amount = value > 0 ? value : 1
+      }
+    }
+	},
+  created () {
+    const localCart = localStorage.getItem('cart')
+    const localTheme = localStorage.getItem('darkTheme')
+    if (localCart) {
+      this.cart = JSON.parse(localCart)
+    }
+    if (localTheme) {
+      this.theme = localTheme
+      document.documentElement.dataset.theme = this.theme
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+@import '~@/style/_main.scss';
+
+button {
+  margin: 6px 0;
+}
+
+.theme-button {
+  position:  absolute;
+  right:     max(calc((100vw - 1008px) / 2), 8px);
+  top:       12px;
+  padding:   3px;
+  min-width: 32px;
+  height:    32px;
+
+  & img {
+    width:  24px;
+    height: 24px;
+  }
+}
+
+.cart {
+  display:          flex;
+  flex-flow:        column;
+  background-color: var(--card-background);
+  border:           var(--card-border);
+  padding:          11px 15px;
+  box-shadow:       0 6px 12px #00000044;
+  max-width:        100%;
+  gap:              8px;
+
+  &__add-button {
+    width: fit-content;
+  }
+
+  &__table {
+    background-color: var(--table);
+    width:            100%;
+    border:           1px solid var(--table-border);
+    border-collapse:  collapse;
+
+    tr,
+    td, {
+      border:          1px solid var(--table-border);
+      border-collapse: collapse;
+      align-items:     center;
+      padding:         4px 12px;
+
+      .select {
+        display: inline-flex;
+      }
+    }
+
+    td:not(:first-child) {
+      width: 0;
+    }
+
+    thead {
+      background-color: var(--table-header);
+      font-size:        14px;
+    }
+
+    tbody {
+      tr:nth-child(2n) {
+        background-color: var(--table-nth-child);
+      }
+
+      tr:last-child {
+        background-color: var(--table-header);
+        font-size:        14px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="select"
+       tabindex="0"
+       @wheel.passive="scroll($event)"
+       @click="openDropdown"
+       @keyup="(event) => {
+         event.key === ' ' || event.key === 'Enter'
+         ? openDropdown(event)
+         : scroll(event)
+       }">
+    <slot name="selected" :selected="selected">
+      <div class="select__selected-option">
+        {{ selected }}
+      </div>
+    </slot>
+    <div class="select__icon">
+      <img src="@/assets/elements/triangle.svg" alt="Dropdown">
+    </div>
+    <div class="select__options" ref="divOptions" v-show="open" @click.stop @keyup.stop.esc="openDropdown($event)">
+      <div class="select__option"
+           tabindex="0"
+           v-for="option of options"
+           :key="key ? option[key] : option"
+           @keydown.enter.space="select(option)"
+           @click="select(option); openDropdown($event)">
+        <slot name="option" :option="option">
+          {{ option }}
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BaseSelect',
+  data: (): { open: boolean, selected: unknown } => ({
+    open: false,
+    selected: ''
+  }),
+  props: {
+    default: {
+      type: [ String, Number ],
+      default: 0
+    },
+    options: {
+      type: Array,
+      default: () => ([])
+    },
+    key: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    select (selected: unknown): void {
+      this.selected = selected
+      this.$emit('select', selected)
+    },
+    openDropdownHandler (): void {
+      this.open = false
+    },
+    openDropdown (event: MouseEvent): void {
+      event.stopImmediatePropagation()
+      this.open = !this.open
+      if (this.open) {
+        document.addEventListener('click', this.openDropdownHandler, { once: true })
+      } else {
+        document.removeEventListener('click', this.openDropdownHandler)
+      }
+    },
+    scroll (event: WheelEvent & KeyboardEvent) {
+      if (this.options && this.options.length) {
+        const index = this.options.indexOf(this.selected)
+
+        if (event.type === 'wheel') {
+          if (event.deltaY < 0) {
+            this.select(this.options[index - 1 >= 0 ? index - 1 : this.options.length - 1])
+          } else {
+            this.select(this.options[index + 1 < this.options.length ? index + 1 : 0])
+          }
+        } else if (event.type === 'keyup') {
+          if (event.key === 'ArrowUp') {
+            this.select(this.options[index - 1 >= 0 ? index - 1 : this.options.length - 1])
+          } else if (event.key === 'ArrowDown') {
+            this.select(this.options[index + 1 < this.options.length ? index + 1 : 0])
+          }
+        }
+      }
+    }
+  },
+  emits: ['select'],
+  created () {
+    if (this.options && this.options.length) {
+      this.selected = typeof this.default === 'number' ? this.options[this.default] : this.default
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.select {
+  position:         relative;
+  display:          flex;
+  align-items:      center;
+  width:            fit-content;
+  min-width:        72px;
+  height:           24px;
+  border:           var(--button-border);
+  outline:          0 solid transparent;
+  background-color: var(--button);
+  border-radius:    3px;
+  user-select:      none;
+
+  &:focus {
+    border:  var(--button-border-focus);
+    outline: var(--button-outline);
+  }
+
+  &:disabled {
+    color:            var(--disabled);
+    outline:          0 solid transparent;
+    background-color: var(--button-disabled);
+  }
+
+  &__icon {
+    width:       22px;
+    height:      22px;
+    padding:     3px;
+    border-left: var(--button-border);
+  }
+
+  &__options {
+    display:          flex;
+    flex-direction:   column;
+    position:         absolute;
+    background-color: var(--button);
+    border:           var(--button-border);
+    padding:          4px 0;
+    gap:              1px;
+    height:           fit-content;
+    max-height:       500%;
+    top:              calc(100%);
+    left:             -1px;
+    min-width:        calc(100% + 2px);
+    z-index:          1;
+  }
+
+  &__option {
+    display:          flex;
+    padding:          0 12px;
+    align-items:      center;
+    height:           22px;
+    background-color: var(--button);
+    outline:          none;
+
+    &:hover,
+    &:focus {
+      background-color: var(--button-disabled);
+    }
+  }
+
+  &__selected-option {
+    margin: 0 12px;
+  }
+}
+</style>
